1c927595fb8dadcceb62e6cefec7c6ed5959c798
maxwell-penguin <maxwellpeng18@gmail.com>
2026-09-13 16:37:22 -0400

    fix(ios): add EverFresh project files properly (remove nested git repo)

---
+++ EverFresh/EverFresh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EverFresh
+//
+//  Created by Maxwell  Peng  on 2026-09-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EverFresh/EverFresh/EverFreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverFreshApp.swift
+//  EverFresh
+//
+//  Created by Maxwell  Peng  on 2026-09-13.
+//
+
+import SwiftUI
+
+@main
+struct EverFreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
